2df887a4f24a8a7597e7d7d4f96e81665dd15648
Yingying Feng <yfeng59@my.bcit.ca>
2022-06-05 22:08:18 -0700

    added ejs

--- express_server.js
+++ express_server.js
@@ -19,9 +19,17 @@ app.listen(PORT, () => {
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-
 });
 
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  };
+  res.render("urls_show", templateVars);
 });
